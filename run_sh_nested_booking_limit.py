41f7a6cb1539e6d4d53f1e21b3cc073c4615fe49
Luoxiaogan <1601710607@qq.com>
2025-02-22 21:36:27 +0800

    nice output of modified nested

--- run_sh_nested_booking_limit.py
+++ run_sh_nested_booking_limit.py
@@ -2,24 +2,24 @@ from utils import run_nested
 
 import subprocess
 
-destination_folder = "./results_analysis/test31/modified_nested"
+destination_folder = "./results_analysis/test32/modified_nested"
 
 prompt_types=[
-{"type": "type1", "prefill": 60, "decode": 50, "arrival_rate": 23},
-{"type": "type2", "prefill": 60, "decode": 100, "arrival_rate": 11},
-{"type": "type3", "prefill": 60, "decode": 150, "arrival_rate": 8},
-{"type": "type4", "prefill": 60, "decode": 200, "arrival_rate": 7},
-{"type": "type5", "prefill": 60, "decode": 250, "arrival_rate": 6},
-{"type": "type6", "prefill": 60, "decode": 300, "arrival_rate": 4},
-{"type": "type7", "prefill": 60, "decode": 350, "arrival_rate": 3},
-{"type": "type8", "prefill": 60, "decode": 400, "arrival_rate": 2},
-{"type": "type9", "prefill": 60, "decode": 450, "arrival_rate": 1},
-{"type": "type10", "prefill": 60, "decode": 500, "arrival_rate": 1},
+{"type": "type1", "prefill": 60, "decode": 50, "arrival_rate": 230},
+{"type": "type2", "prefill": 60, "decode": 100, "arrival_rate": 110},
+{"type": "type3", "prefill": 60, "decode": 150, "arrival_rate": 80},
+{"type": "type4", "prefill": 60, "decode": 200, "arrival_rate": 70},
+{"type": "type5", "prefill": 60, "decode": 250, "arrival_rate": 60},
+{"type": "type6", "prefill": 60, "decode": 300, "arrival_rate": 40},
+{"type": "type7", "prefill": 60, "decode": 350, "arrival_rate": 30},
+{"type": "type8", "prefill": 60, "decode": 400, "arrival_rate": 20},
+{"type": "type9", "prefill": 60, "decode": 450, "arrival_rate": 10},
+{"type": "type10", "prefill": 60, "decode": 500, "arrival_rate": 10},
 ]
 
 run_nested(
     destination_folder = destination_folder,
-    limit_start = 120,
+    limit_start = 115,
     limit_end = 175,
     limit_interval = 5,
     num_requests = 8000,
--- run_sh_vllm.py
+++ run_sh_vllm.py
@@ -1,21 +1,21 @@
 from utils import copy_latest_csv, run_modified, run_sarathi, run_vllm
 
-destination_folder = "/Users/luogan/Code/vidur_or/results_analysis/test31/vllm"
+destination_folder = "/Users/luogan/Code/vidur_or/results_analysis/test32/vllm"
 
 prompt_types=[
-{"type": "type1", "prefill": 60, "decode": 50, "arrival_rate": 23},
-{"type": "type2", "prefill": 60, "decode": 100, "arrival_rate": 11},
-{"type": "type3", "prefill": 60, "decode": 150, "arrival_rate": 8},
-{"type": "type4", "prefill": 60, "decode": 200, "arrival_rate": 7},
-{"type": "type5", "prefill": 60, "decode": 250, "arrival_rate": 6},
-{"type": "type6", "prefill": 60, "decode": 300, "arrival_rate": 4},
-{"type": "type7", "prefill": 60, "decode": 350, "arrival_rate": 3},
-{"type": "type8", "prefill": 60, "decode": 400, "arrival_rate": 2},
-{"type": "type9", "prefill": 60, "decode": 450, "arrival_rate": 1},
-{"type": "type10", "prefill": 60, "decode": 500, "arrival_rate": 1},
+{"type": "type1", "prefill": 60, "decode": 50, "arrival_rate": 230},
+{"type": "type2", "prefill": 60, "decode": 100, "arrival_rate": 110},
+{"type": "type3", "prefill": 60, "decode": 150, "arrival_rate": 80},
+{"type": "type4", "prefill": 60, "decode": 200, "arrival_rate": 70},
+{"type": "type5", "prefill": 60, "decode": 250, "arrival_rate": 60},
+{"type": "type6", "prefill": 60, "decode": 300, "arrival_rate": 40},
+{"type": "type7", "prefill": 60, "decode": 350, "arrival_rate": 30},
+{"type": "type8", "prefill": 60, "decode": 400, "arrival_rate": 20},
+{"type": "type9", "prefill": 60, "decode": 450, "arrival_rate": 10},
+{"type": "type10", "prefill": 60, "decode": 500, "arrival_rate": 10},
 ]
 
-batch_size_list = [115,119,126,130,134,139,148,155,159,164]
+batch_size_list = [110,115,119,126,130,139,145,148,155,159,164]
 
 run_vllm(
     destination_folder = destination_folder,
